eaf059709c551978c4119c6a37f9668fbfe0752b
Oliver Kramer <3520_kramer@live.dk>
2020-04-22 11:06:56 +0200

    commented code

--- 07_decorators_week13/small_log_exercise(simple_decorator).py
+++ 07_decorators_week13/small_log_exercise(simple_decorator).py
@@ -12,21 +12,28 @@ or prints the text 2 times. Decorate it with your logfunction. Does it work? YES
 
 import datetime
 
-
+# When we call the add function, the wrapper will also log the time in a seperate file
 def my_decorator(func):
     def wrapper(*args):
+        # opens a file in append mode
         file = open("log.txt", 'a')
 
+        # calling the function with the arguments
         func(*args)
 
+        # saving the text input and the time in a varibale for the log
         argument_and_time = f'{args} = {func(*args)} @ {datetime.datetime.now()}'
 
+        # writes the log to the file
         file.write(f'{argument_and_time}\n')
         
+        # closes the file
+        file.close()
         return argument_and_time
 
     return wrapper
 
+# decorator annotation
 @my_decorator
 def add(*args):
     sum = 0
@@ -34,6 +41,8 @@ def add(*args):
         sum += i
     return sum
 
+# function which returns the text entered two times
+# it works with the decorator but the output printed to the log is not ideal
 @my_decorator
 def printer(text):
     return f'{text} {text}'
